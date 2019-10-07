ca5d046735dbd6a5cd8e3ea9840110a7e8d0259b
TeamDman <TeamDman9201@gmail.com>
2019-10-07 01:48:11 -0400

    cpprestsdk test

---
+++ backend/web/server.h
@@ -0,0 +1,17 @@
+//
+// Created by User on 2019-10-07.
+//
+
+#ifndef AVATAR_SERVER_H
+#define AVATAR_SERVER_H
+
+
+class server {
+public:
+    void start();
+
+    void fetch();
+};
+
+
+#endif //AVATAR_SERVER_H
