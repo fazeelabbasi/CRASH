fa6c71f87f042e09c51003237589856f776afe81
TeamDman <TeamDman9201@gmail.com>
2019-10-07 00:42:28 -0400

    Basic HelloWorld for backend

---
+++ backend/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by User on 2019-10-07.
+//
+
+#include "main.h"
+#include <iostream>
+
+
+int main() {
+    std::cout << "Hello world!";
+}
---
+++ backend/main.h
@@ -0,0 +1,14 @@
+//
+// Created by User on 2019-10-07.
+//
+
+#ifndef AVATAR_MAIN_H
+#define AVATAR_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //AVATAR_MAIN_H
