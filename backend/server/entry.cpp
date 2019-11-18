59ec9fa255754f464c673118c2b47728cc4c3332
TeamDman <TeamDman9201@gmail.com>
2019-11-18 15:49:54 -0500

    Switch hook to method, fix whitespace

--- backend/server/entry.cpp
+++ backend/server/entry.cpp
@@ -12,88 +12,90 @@ Server s(2878);
 
 
 void * asyncSend(void *raw) {
-    struct Server::Connector *args = (struct Server::Connector *) raw;
-    for(;;) {
-        usleep(5000000);
-        std::cout << "sending woohoo" << std::endl;
-        s.sendMessage(*args, "woohoo");
-    }
-    free(args);
+	struct Server::Connector *args = (struct Server::Connector *) raw;
+	for(;;) {
+		usleep(5000000);
+		std::cout << "sending woohoo" << std::endl;
+		s.sendMessage(*args, "woohoo");
+	}
+	free(args);
 }
 
 void onInput(uint16_t df, char *buffer) {
-    std::cout << "[" << df << "]\t<" << buffer << ">" << std::endl; 
-    // Connector c = 
-    s.sendMessage((struct Server::Connector){.source_fd=df}, "yeet");
+	std::cout << "[" << df << "]\t<" << buffer << ">" << std::endl; 
+	// Connector c = 
+	s.sendMessage((struct Server::Connector){.source_fd=df}, "yeet");
+}
+
+void onDisconnect(uint16_t df) {
+	std::cout << df << " disconnected." << std::endl;
 }
 
 void onConnect(uint16_t df) {
-    std::cout << df << " connected." << std::endl;
-    pthread_t t;
-    struct Server::Connector *args = (Server::Connector *) malloc(sizeof(struct Server::Connector));
-    args->source_fd=df;
-    if (pthread_create(&t, NULL, asyncSend, (void * ) args) != 0){
-        printf("shit's fucked yo\n");
-    }
+	std::cout << df << " connected." << std::endl;
+	pthread_t t;
+	struct Server::Connector *args = (Server::Connector *) malloc(sizeof(struct Server::Connector));
+	args->source_fd=df;
+	if (pthread_create(&t, NULL, asyncSend, (void * ) args) != 0){
+		printf("shit's fucked yo\n");
+	}
 }
 
 int main() {
-    std::cout << "howdy" << std::endl;
-    s.onConnect(&onConnect);
-    // MyHandler mh;
-    s.onInput(&onInput);
-    s.onDisconnect([](uint16_t df) {
-        std::cout << df << " disconnected." << std::endl;
-    });
-    s.init();
-    for(;;) s.loop();
-    std::cout << "we done now " << std::endl;
-    return 0;
+	std::cout << "Launching server" << std::endl;
+	s.onConnect(&onConnect);
+	s.onDisconnect(&onDisconnect);
+	s.onInput(&onInput);
+
+	s.init();
+	for(;;) s.loop();
+	std::cout << "we done now " << std::endl;
+	return 0;
 }
 
 
 int roundTest()
 {
-    RoundLogic test;
-    test.roundsPlayed++;
-    test.roundTimer = 0;
-    test.roundStatus = "warmup";
-    test.clientsFinished = 0;
-    test.validClients = 0;
-    test.clientLogin("asdf");
-    test.clientLogin("qwerty");
-    Player asdfC = test.loggedInUsers[0];
-    Player qwertyC = test.loggedInUsers[1];
-    this_thread::sleep_for(chrono::seconds(2));
-    for (int i = 0; i < test.loggedInUsers.size(); i++) {
-        cout << "status: " << test.loggedInUsers[i].getStatus() << endl;
-        if(test.loggedInUsers[i].getStatus() == "IN") {
-            cout << "Valid client" << endl;
-            test.validClients++;
-            test.loggedInUsers[i].setStatus("OUT");
-        }
-    }
-    test.roundStatus = "begin";
-    asdfC.updateMoney((double)-10000);
-    qwertyC.updateMoney((double)150);
-    test.clientUpdate(qwertyC);
-    test.clientUpdate(asdfC);
-    while(test.clientsFinished < test.validClients && test.roundTimer < 2) {
-        this_thread::sleep_for(chrono::seconds(1));
-        test.roundTimer++;
-    }
+	RoundLogic test;
+	test.roundsPlayed++;
+	test.roundTimer = 0;
+	test.roundStatus = "warmup";
+	test.clientsFinished = 0;
+	test.validClients = 0;
+	test.clientLogin("asdf");
+	test.clientLogin("qwerty");
+	Player asdfC = test.loggedInUsers[0];
+	Player qwertyC = test.loggedInUsers[1];
+	this_thread::sleep_for(chrono::seconds(2));
+	for (int i = 0; i < test.loggedInUsers.size(); i++) {
+		cout << "status: " << test.loggedInUsers[i].getStatus() << endl;
+		if(test.loggedInUsers[i].getStatus() == "IN") {
+			cout << "Valid client" << endl;
+			test.validClients++;
+			test.loggedInUsers[i].setStatus("OUT");
+		}
+	}
+	test.roundStatus = "begin";
+	asdfC.updateMoney((double)-10000);
+	qwertyC.updateMoney((double)150);
+	test.clientUpdate(qwertyC);
+	test.clientUpdate(asdfC);
+	while(test.clientsFinished < test.validClients && test.roundTimer < 2) {
+		this_thread::sleep_for(chrono::seconds(1));
+		test.roundTimer++;
+	}
 
-    for(int i = 0; i < test.loggedInUsers.size(); i++){
-        if(test.loggedInUsers[i].getStatus() == "OUT"){
-            test.loggedInUsers.erase(test.loggedInUsers.begin() + i);
-        }
-    }
+	for(int i = 0; i < test.loggedInUsers.size(); i++){
+		if(test.loggedInUsers[i].getStatus() == "OUT"){
+			test.loggedInUsers.erase(test.loggedInUsers.begin() + i);
+		}
+	}
 
-    for(int i = 0; i < test.loggedInUsers.size(); i++){
-        test.loggedInUsers[i].updateIndex(i);
-    }
+	for(int i = 0; i < test.loggedInUsers.size(); i++){
+		test.loggedInUsers[i].updateIndex(i);
+	}
 
-    cout << test.loggedInUsers[0].getMoney() << endl;
-    cout << test.loggedInUsers[0].getName() << endl;
-    cout << test.loggedInUsers[0].getIndex() << endl;
+	cout << test.loggedInUsers[0].getMoney() << endl;
+	cout << test.loggedInUsers[0].getName() << endl;
+	cout << test.loggedInUsers[0].getIndex() << endl;
 }
